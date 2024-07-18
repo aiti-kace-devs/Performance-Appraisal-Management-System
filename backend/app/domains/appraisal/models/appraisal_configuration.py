be48b31adedb1cdb32a22951f2d0853d175a224d
Samuel Kusi-Duah <kduah54@gmail.com>
2024-07-18 12:14:44 +0000

    GIKPA-237-Create-appraisal-configuration-model

---
+++ backend/app/domains/appraisal/models/appraisal_configuration.py
@@ -0,0 +1,21 @@
+from typing import Any
+from sqlalchemy import JSON, Boolean, Column, Date, DateTime, ForeignKey, String, Text,Integer
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from db.base_class import APIBase
+
+
+
+
+
+class AppraisalConfiguration(APIBase):
+
+    appraisal_cycles_id = Column(UUID(as_uuid=True), ForeignKey('appraisal_cycles.id'), nullable=True) 
+    configuration = Column(JSON, nullable=False)
+    
+
+
+    
+
+
+
