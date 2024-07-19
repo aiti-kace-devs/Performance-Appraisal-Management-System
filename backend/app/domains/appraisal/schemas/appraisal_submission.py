9e7f226363775f6290939015c59518e36898aa8c
Ajara Oyinloye <lawalajara1@gmail.com>
2024-07-19 14:53:10 +0000

    schemas editted

--- backend/app/domains/appraisal/schemas/appraisal_submission.py
+++ backend/app/domains/appraisal/schemas/appraisal_submission.py
@@ -3,17 +3,34 @@ from typing import Optional, Any, Dict
 
 from pydantic import BaseModel
 from pydantic import UUID4
+from sqlalchemy import JSON
 
 
 class AppraisalSubmissionBase(BaseModel):
-    name:str
-    description: Optional[str] = None
-    created_at: date
+    appraisals_id : UUID4 
+    staffs_id : UUID4
+    submitted_values : JSON
+    started_at : date
+    completed_at : date
+    submitted : bool
+    completed : bool
+    approval_status : bool
+    approval_date : date
+    appraisal_forms_id : UUID4
+    comment : str
 
 class AppraisalSubmissionCreate(AppraisalSubmissionBase):
-    name:str
-    description: Optional[str] = None
-    created_at: date
+    appraisals_id : UUID4 
+    staffs_id : UUID4
+    submitted_values : JSON
+    started_at : date
+    completed_at : date
+    submitted : bool
+    completed : bool
+    approval_status : bool
+    approval_date : date
+    appraisal_forms_id : UUID4
+    comment : str
 
 class AppraisalSubmissionUpdate(AppraisalSubmissionBase):
     pass
