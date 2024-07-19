900e6e514187fa26ae66b1451b0dec0718789621
Ajara Oyinloye <lawalajara1@gmail.com>
2024-07-19 14:12:04 +0000

    removed foreign key attribute,commentted relationship

--- backend/app/domains/appraisal/models/appraisal_submission.py
+++ backend/app/domains/appraisal/models/appraisal_submission.py
@@ -8,18 +8,18 @@ from db.base_class import APIBase
 
 class AppraisalSubmission(APIBase):
 
-    appraisals_id = Column(UUID(as_uuid=True), ForeignKey('appraisals.id'), nullable=True)
-    staffs_id = Column(UUID(as_uuid=True), ForeignKey('staffs.id'), nullable=True)
+    appraisals_id = Column(UUID(as_uuid=True))
+    staffs_id = Column(UUID(as_uuid=True))
     submitted_values = Column(JSON, nullable=False)
-    started_at = Column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
-    completed_at = Column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
+    started_at = Column(DateTime)
+    completed_at = Column(DateTime)
     submitted = Column(Boolean, default=True)
     completed = Column(Boolean, default=True)
     approval_status = Column(Boolean, default=True)
-    approval_date = Column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
-    appraisal_forms_id = Column(UUID(as_uuid=True), ForeignKey('appraisal_forms.id'), nullable=True)
+    approval_date = Column(DateTime)
+    appraisal_forms_id = Column(UUID(as_uuid=True))
     comment = Column(String(255),nullable=True)
 
-    appraisals = relationship("Appraisal", back_populates = "appraisal_submissions")
-    staffs = relationship("Staff", back_populates = "appraisal_submissions")
-    appraisal_forms = relationship("AppraisalForm", back_populates = "appraisal_submissions") 
+    #appraisals = relationship("Appraisal", back_populates = "appraisal_submissions")
+    #staffs = relationship("Staff", back_populates = "appraisal_submissions")
+    #appraisal_forms = relationship("AppraisalForm", back_populates = "appraisal_submissions") 
