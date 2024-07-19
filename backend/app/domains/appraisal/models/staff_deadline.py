838e978b0b610530b3bee62e0315ae7f4a55464c
GI-KACE-DEV <dev.aiti.com.gh@gmail.com>
2024-07-19 14:58:57 +0000

    GIKPA-241-Create-staff-deadline-model

--- backend/app/domains/appraisal/models/staff_deadline.py
+++ backend/app/domains/appraisal/models/staff_deadline.py
@@ -1,5 +1,5 @@
 from typing import Any
-from sqlalchemy import JSON, Boolean, Column, Date, DateTime, ForeignKey, String, Text,Integer
+from sqlalchemy import Column, Date, DateTime, ForeignKey, String, Text,Integer
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from db.base_class import APIBase
@@ -11,6 +11,6 @@ class StaffDeadline(APIBase):
     appraisal_sections_id = Column(UUID(as_uuid=True))
     staffs_id = Column(UUID(as_uuid=True))
     supervisor_id = Column(UUID(as_uuid=True))
-    start_date = Column(String, nullable=True)
-    end_date = Column(String(255), nullable=True)
+    start_date = Column(DateTime, nullable=True)
+    end_DateTime = Column(Date, nullable=True)
     comment = Column(String(255), nullable=True)
---
+++ backend/app/domains/appraisal/schemas/staff_deadline.py
@@ -0,0 +1,74 @@
+from pydantic import UUID4,BaseModel,Field,field_validator
+from datetime import datetime,date
+from dateutil.parser import parse
+from typing import Optional
+import uuid
+
+
+
+class StaffDeadlineBase(BaseModel):
+    appraisal_sections_id: Optional[UUID4]
+    staffs_id: Optional[UUID4]
+    supervisor_id: Optional[UUID4]
+    start_date:Optional[date]
+    end_date: Optional[date]
+    comment: Optional[str]
+
+
+
+class StaffDeadlineCreate(StaffDeadlineBase):
+    appraisal_sections_id: Optional[UUID4] 
+    staffs_id: Optional[UUID4]
+    supervisor_id: Optional[UUID4]
+    start_date:Optional[date]
+    end_date: Optional[date]
+    comment: Optional[str]= Field(..., min_length=1) # Ensure comment is not empty
+
+
+
+    # Checking if fields are not empty and also not allowing the word string as value
+    @field_validator('appraisal_sections_id', 'staffs_id', 'supervisor_id', 'comment', mode='before')
+    def check_non_empty_and_not_string(cls, v, info):
+        if isinstance(v, str) and (v.strip() == '' or v.strip().lower() == 'string'):
+            raise ValueError(f'\n{info.field_name} should not be empty or the word "string"')
+        return v
+    
+
+    # Checking if UUID4 fields accept only UUID4 as value
+    @field_validator('appraisal_sections_id', 'staffs_id', 'supervisor_id', mode='before')
+    def validate_fields_with_uuid4(cls, v, info):
+        try:
+            uuid.UUID(v, version=4)
+        except ValueError:
+            raise ValueError(f'\n{info.field_name} must have a valid UUID4')
+        return v
+
+
+    # Checking if start_date and end_date is none, current time nad date will be submitted
+    @field_validator('start_date', 'end_date', mode='before')
+    def validate_and_convert_date_format(cls, v, info):
+        if v is not None:
+            try:
+                dt = parse(v)
+                now = datetime.now()
+                combined_datetime = dt.replace(hour=now.hour, minute=now.minute, second=now.second)
+                formatted_date = combined_datetime.strftime('%Y-%m-%dT%H:%M:%S')
+                return formatted_date
+            except ValueError:
+                raise ValueError(f'\n{info.field_name} must be a valid date format and will be converted to YYYY-MM-DDTHH:MM:SS')
+        return v
+
+
+
+
+class StaffDeadlineUpdate(StaffDeadlineBase):
+    pass
+
+class StaffDeadlineInDBBase(StaffDeadlineBase):
+    id: UUID4
+
+    class Config:
+        orm_mode= True
+
+class StaffDeadlineSchema(StaffDeadlineBase):
+    pass
