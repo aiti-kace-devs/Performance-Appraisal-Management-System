927c2fa12eb77bf76e67722ba1ace2ab3c7dc378
HG-MESSIAH <shonbismark552@gmail.com>
2024-07-22 13:28:54 +0000

    make validations for users

--- backend/app/domains/appraisal/schemas/users.py
+++ backend/app/domains/appraisal/schemas/users.py
@@ -1,7 +1,7 @@
 from datetime import date,time
 from typing import Optional, Any, Dict
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from pydantic import UUID4
 
 
@@ -18,6 +18,15 @@ class UserCreate(UserBase):
     password:str
     reset_password_token:str
     created_at:date
+
+@field_validator('name','description', mode='before')
+def check_non_empty_and_not_string(cls,v,info):
+    if isinstance(v,str) and (v.strip() == '' or v.strip().lower() == 'string'):
+        raise ValueError(f'\n{info.field_name} should not be empty "string"')
+    #make minimum value 1
+    if len(v.strip()) < 1:
+        raise ValueError(f'{info.field_name} should have a minimum value of 1')
+    return v
 
 class UserUpdate(UserBase):
     pass
