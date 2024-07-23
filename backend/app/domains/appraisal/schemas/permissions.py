2be93f6899f878d2c2a57998ae64c6db00f9f574
moro23 <billhab66@gmail.com>
2024-07-23 17:54:11 +0000

    updated permissions schemas file.

--- backend/app/domains/appraisal/schemas/permissions.py
+++ backend/app/domains/appraisal/schemas/permissions.py
@@ -6,7 +6,7 @@ class PermissionBase(BaseModel):
 
     @validator('name')
     def name_must_not_be_empty(cls, value):
-        if not value or value.isspace():
+        if not value or value.isspace() or (value.lower() == "string"):
             raise ValueError("Permission name must not be empty or only whitespace")
         return value
 
