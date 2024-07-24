1ce32b275dc6362e1192aa82d8d999ab8f6adc1c
moro23 <billhab66@gmail.com>
2024-07-24 15:20:29 +0000

    added roles model.

--- backend/app/domains/appraisal/models/roles.py
+++ backend/app/domains/appraisal/models/roles.py
@@ -1,7 +1,7 @@
 import datetime
 from typing import Any
 import uuid
-from sqlalchemy import JSON, Boolean, Column, Date, DateTime, ForeignKey, String, Text,Integer
+from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from db.base_class import APIBase
@@ -10,9 +10,19 @@ from db.base_class import APIBase
 
 
 class Role(APIBase):
-    name = Column(String(255),nullable=False)
-    description = Column(String(255),nullable=True)
+    __tablename__ = "roles"
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, nullable=False, default=uuid.uuid4)
+    name = Column(String(255), unique=True, index=True)
+    role_permissions = relationship("RolePermission", back_populates="roles")
+    users = relationship("User", back_populates="roles")
+   
 
-    users = relationship("User",back_populates="roles")
+    permissions = relationship(
+        "Permission",
+        secondary="role_permissions",
+        back_populates="roles",
+        #overlaps="role_permissions, role,permission"
+    )
+    
 
 
