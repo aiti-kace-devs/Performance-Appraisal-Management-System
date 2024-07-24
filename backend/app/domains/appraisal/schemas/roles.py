9ead2b0408a5dd25e7996ed407a92af92d1c0b53
moro23 <billhab66@gmail.com>
2024-07-24 17:21:15 +0000

    updated schemas file.

--- backend/app/domains/appraisal/schemas/roles.py
+++ backend/app/domains/appraisal/schemas/roles.py
@@ -8,8 +8,8 @@ class RoleBase(BaseModel):
 
     @validator('name')
     def name_must_not_be_empty(cls, value):
-        if not value or value.isspace():
-            raise ValueError("Role name must not be empty or only whitespace")
+        if not value or value.isspace() or (value.lower() == 'string'):
+            raise ValueError("Role name must not be empty or only whitespace or string")
         return value
 
 class RoleCreate(RoleBase):
@@ -30,44 +30,9 @@ class RoleUpdate(RoleBase):
             raise ValueError("Invalid permission data")
         return value
 
-class Role(RoleBase):
+class ReadRole(RoleBase):
     id: int
     permissions: List[Permission] = []
 
     class Config:
         orm_mode = True
-
-# class RoleBase(BaseModel):
-#     name:str
-#     description: Optional[str] = None
-
-
-# class RoleCreate(RoleBase):
-#     name:str
-#     description: Optional[str] = None
-    
-#     @field_validator('name','description', mode='before')
-#     def check_non_empty_and_not_string(cls,v,info):
-#         if isinstance(v,str) and (v.strip() == '' or v.strip().lower() == 'string'):
-#             raise ValueError(f'\n{info.field_name} should not be empty "string"')
-        
-#         #make minimum value 1
-#         if len(v.strip()) < 1:
-#             raise ValueError(f'{info.field_name} should have a minimum value of 1')
-
-#         return v
-    
-    
-
-
-# class RoleUpdate(RoleBase):
-#     pass
-
-# class RoleInDBBase(RoleBase):
-#     id: UUID4
-
-#     class Config:
-#         orm_mode= True
-
-# class RoleSchema(RoleBase):
-#     pass
