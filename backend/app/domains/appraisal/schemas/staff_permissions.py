05411f2c1f34e4f303d2841a6477a475dc4d2967
Jude Clottey <judeclottey1@gmail.com>
2024-07-24 17:11:36 +0000

    Created validation for Staff_Permission Schema

--- backend/app/domains/appraisal/schemas/staff_permissions.py
+++ backend/app/domains/appraisal/schemas/staff_permissions.py
@@ -1,18 +1,31 @@
 from datetime import date,time
 from typing import Optional, Any, Dict
-from pydantic import BaseModel,UUID4
-
-
+from pydantic import BaseModel,UUID4, field_validator, model_validator
+import uuid
 
 class StaffPermissionBase(BaseModel):
     staffs_id : UUID4
     roles_id : UUID4
     permissions_id : UUID4
 
-class StaffPermissionCreate(BaseModel):
-    staffs_id : UUID4
-    roles_id : UUID4
-    permissions_id : UUID4
+class StaffPermissionCreate(StaffPermissionBase):
+    pass
+
+    # Checking if fields are not empty and also not allowing the word string as value
+    @field_validator('staffs_id', 'roles_id', 'permissions_id', mode='before')
+    def check_non_empty_and_not_string(cls, v, info):
+        if isinstance(v, str) and (v.strip() == '' or v.strip().lower() == 'string'):
+            raise ValueError(f'\n{info.field_name} should not be empty or the word "string"')
+        return v
+
+    # Checking if UUID4 fields accept only UUID4 as value
+    @field_validator('staffs_id', 'roles_id', 'permissions_id', mode='before')
+    def validate_fields_with_uuid4(cls, v, info):
+        try:
+            uuid.uuid4(v)
+        except ValueError:
+            raise ValueError(f'\n{info.field_name} must have a valid UUID4')
+        return v
 
 class StaffPermissionUpdate(StaffPermissionBase):
     pass
