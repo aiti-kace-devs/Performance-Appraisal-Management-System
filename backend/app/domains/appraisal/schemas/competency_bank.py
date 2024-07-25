3a74c23b0ddd863ef9250c1d1f24ddc1abdaa34c
HG-MESSIAH <shonbismark552@gmail.com>
2024-07-25 06:49:59 +0000

    added uuid validator check

--- backend/app/domains/appraisal/schemas/competency_bank.py
+++ backend/app/domains/appraisal/schemas/competency_bank.py
@@ -1,7 +1,8 @@
 from datetime import date,time
 from typing import Optional, Any, Dict
+import uuid
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from pydantic import UUID4
 from sqlalchemy import JSON
 
@@ -15,6 +16,15 @@ class Compentency_BankCreate(Compentency_BankBase):
     appraisal_section_id = Optional[UUID4]
     staff_id = Optional[UUID4]
     compentency_type = JSON
+
+     # Checking if UUID4 fields accept only UUID4 as value
+    @field_validator('appraisal_sections_id', 'staffs_id', mode='before')
+    def validate_fields_with_uuid4(cls, v, info):
+        try:
+            uuid.UUID(v, version=4)
+        except ValueError:
+            raise ValueError(f'\n{info.field_name} must have a valid UUID4')
+        return v
 
 
 class Compentency_BankUpdate(Compentency_BankBase):
