ed50ddeee9586960a448307d7a87b52b5fb8149c
moro23 <billhab66@gmail.com>
2024-07-25 12:24:12 +0000

    updated roles model.

--- backend/app/domains/appraisal/models/roles.py
+++ backend/app/domains/appraisal/models/roles.py
@@ -10,8 +10,6 @@ from db.base_class import APIBase
 
 
 class Role(APIBase):
-    __tablename__ = "roles"
-    id = Column(UUID(as_uuid=True), primary_key=True, index=True, nullable=False, default=uuid.uuid4)
     name = Column(String(255), unique=True, index=True)
     role_permissions = relationship("RolePermission", back_populates="roles")
     users = relationship("User", back_populates="roles")
