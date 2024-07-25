97054c592eafcea25dd624a3208fa5aa47168e18
Ajara Oyinloye <lawalajara1@gmail.com>
2024-07-25 09:07:37 +0000

    Added validation

--- backend/app/domains/appraisal/schemas/appraisal.py
+++ backend/app/domains/appraisal/schemas/appraisal.py
@@ -1,7 +1,8 @@
 from datetime import date,time
 from typing import Optional, Any, Dict
+import uuid
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from pydantic import UUID4
 
 
@@ -16,6 +17,26 @@ class AppraisalCreate(AppraisalBase):
     staffs_id: UUID4
     supervisor_id: UUID4
     overall_score: str
+
+
+     # Checking if fields are not empty and also not allowing the word string as value
+    @field_validator('appraisal_cycles_id', 'supervisor_id', 'staffs_id', 'overall_score', mode='before')
+    def check_non_empty_and_not_string(cls, value, info):
+        if isinstance(value, str) and (value.strip() == '' or value.strip().lower() == 'string'):
+            raise ValueError(f'\n{info.field_name} should not be empty or the word "string"')
+        return value
+    
+
+     # Checking if UUID4 fields accept only UUID4 as value
+    @field_validator('appraisal_cycles_id', 'staffs_id', 'supervisor_id', mode='before')
+    def validate_fields_with_uuid4(cls, value, info):
+        try:
+            uuid.UUID(value, version=4)
+        except ValueError:
+            raise ValueError(f'\n{info.field_name} must have a valid UUID4')
+        return value
+
+
 
 class AppraisalUpdate(AppraisalBase):
     pass
