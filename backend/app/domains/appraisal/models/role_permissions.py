a883865e71878afefb452a351e5de806dfd06b9e
moro23 <billhab66@gmail.com>
2024-07-25 13:50:42 +0000

    updated role-permissions models.

--- backend/app/domains/appraisal/models/role_permissions.py
+++ backend/app/domains/appraisal/models/role_permissions.py
@@ -1,16 +1,14 @@
 from typing import Any
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from db.base_class import APIBase
-import uuid 
+
 
 
 
 class RolePermission(APIBase):
-    __tablename__ = "role_permissions"
-    id = Column(UUID(as_uuid=True), primary_key=True, index=True, nullable=False, default=uuid.uuid4)
-
+    
     role_id = Column(UUID(as_uuid=True), ForeignKey("roles.id"))
     permission_id = Column(UUID(as_uuid=True), ForeignKey("permissions.id"))
     
