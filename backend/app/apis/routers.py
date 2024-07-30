7b641c3432092b9658cc09fd9560198b9f4fba03
Jude Clottey <judeclottey1@gmail.com>
2024-07-30 14:52:55 +0000

    GIKPA-246-Create-staff-permissions-repo-service-and-API

--- backend/app/apis/routers.py
+++ backend/app/apis/routers.py
@@ -1,6 +1,7 @@
 
 from domains.appraisal.apis.appraisal import appraisal_forms_router
-from fastapi import APIRouter
+from fastapi import APIRouter 
+from domains.appraisal.apis.staff_permissions import staff_permission_router
 
 
 
@@ -8,5 +9,5 @@ from fastapi import APIRouter
 
 router = APIRouter()
 router.include_router(appraisal_forms_router)
-
+router.include_router(staff_permission_router)
 
---
+++ backend/app/domains/appraisal/services/staff_permissions.py
@@ -0,0 +1,42 @@
+from typing import List, Any
+import re
+from fastapi import HTTPException, status
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
+from db.base_class import UUID
+from domains.appraisal.respository.staff_permissions import staff_permission_action as staff_permission_repo
+from domains.appraisal.schemas.staff_permissions import StaffPermissionSchema, StaffPermissionUpdate, StaffPermissionCreate
+from domains.appraisal.models.staff_permissions import StaffPermission
+
+class StaffPermissionService:
+
+
+    def create_staff_permissions(self, *, db: Session, staff_permission:StaffPermissionCreate) -> StaffPermissionSchema:
+        staff_permissions_obj = staff_permission_repo.create(db=db, obj_in = staff_permission)
+        return staff_permissions_obj
+
+    def get_staff_permissions(self, *, db: Session, id: UUID) -> StaffPermissionSchema:
+        staff_permissions_obj = staff_permission_repo.get(db=db, id=id)
+        if not staff_permissions_obj:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="staff_permission not found")
+        return staff_permissions_obj
+    
+    def get_all_staff_permissions(self, *, db: Session, skip: int = 0, limit: int = 100) -> List[StaffPermissionSchema]:
+        staff_permission_obj = staff_permission_repo.get_all(db=db, skip=skip, limit=limit)
+        return staff_permission_obj
+    
+    def update_staff_permissions(self, *, db: Session, id: UUID,  staff_permission: StaffPermissionUpdate) -> StaffPermissionSchema:
+        staff_permissions_obj = staff_permission_repo.get(db=db, id=id)
+        if not staff_permissions_obj:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="staff_permission_id not found")
+        staff_permissions_obj = staff_permission_repo.update(db=db, db_obj=staff_permissions_obj, obj_in=staff_permission)
+        return staff_permissions_obj
+
+    def delete_staff_permissions(self, *, db: Session, id: UUID) -> StaffPermissionSchema:
+        staff_permissions_obj = staff_permission_repo.get(db=db, id=id)
+        if not staff_permissions_obj:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="staff_permissions_id not found")
+        staff_permissions_obj = staff_permission_repo.remove(db=db, id=id)
+        return staff_permissions_obj
+
+staff_permission_service = StaffPermissionService()
