1c3b00fa6b33300b4475190b91c07b54ec10e3eb
Paa-Kay <pkafrifa99@gmail.com>
2024-07-31 12:01:44 +0000

    added routers

--- backend/app/apis/routers.py
+++ backend/app/apis/routers.py
@@ -1,5 +1,7 @@
 
-from domains.appraisal.apis.appraisal_form import appraisal_form_router
+from domains.appraisal.apis.appraisal_form import appraisal_form_router 
+from domains.appraisal.apis.staff import staff_router
+
 from fastapi import APIRouter
 
 
@@ -8,5 +10,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 router.include_router(appraisal_form_router)
+router.include_router(staff_router)
 
 
