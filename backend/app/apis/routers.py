5c9130d06f460d301a23d9ec316632b68c80a77b
HG-MESSIAH <shonbismark552@gmail.com>
2024-07-31 12:02:46 +0000

    added routers

--- backend/app/apis/routers.py
+++ backend/app/apis/routers.py
@@ -1,5 +1,7 @@
 
 from domains.appraisal.apis.appraisal_form import appraisal_form_router
+from domains.appraisal.apis.users import users_router
+from domains.appraisal.apis.competency_bank import competency_bank_forms_router
 from fastapi import APIRouter
 
 
@@ -8,5 +10,8 @@ from fastapi import APIRouter
 
 router = APIRouter()
 router.include_router(appraisal_form_router)
+router.include_router()
+router.include_router(users_router)
+router.include_router(competency_bank_forms_router)
 
 
