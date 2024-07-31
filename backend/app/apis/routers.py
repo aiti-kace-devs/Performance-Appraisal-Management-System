a00386e6488a490aa01123554699e5724f006228
Ajara Oyinloye <lawalajara1@gmail.com>
2024-07-31 12:01:13 +0000

    added onto routers

--- backend/app/apis/routers.py
+++ backend/app/apis/routers.py
@@ -1,5 +1,8 @@
 
 from domains.appraisal.apis.appraisal_form import appraisal_form_router
+from domains.appraisal.apis.appraisal_submission import appraisal_submissions_router
+from domains.appraisal.apis.department import department_router
+from domains.appraisal.apis.appraisal import appraisals_router
 from fastapi import APIRouter
 
 
@@ -8,5 +11,8 @@ from fastapi import APIRouter
 
 router = APIRouter()
 router.include_router(appraisal_form_router)
+router.include_router(appraisal_submissions_router)
+router.include_router(department_router)
+router.include_router(appraisals_router)
 
 
