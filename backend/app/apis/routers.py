c14fde0686b83ec03a54006d58578f41ccfe8ed7
Paa-Kay <pkafrifa99@gmail.com>
2024-07-31 12:12:06 +0000

    added staff to router

--- backend/app/apis/routers.py
+++ backend/app/apis/routers.py
@@ -5,6 +5,7 @@ from domains.appraisal.apis.department import department_router
 from domains.appraisal.apis.appraisal import appraisals_router
 from domains.appraisal.apis.users import users_router
 from domains.appraisal.apis.competency_bank import competency_bank_forms_router
+from domains.appraisal.apis.staff import staff_router
 from fastapi import APIRouter
 
 
@@ -18,4 +19,5 @@ router.include_router(department_router)
 router.include_router(appraisals_router)
 router.include_router(users_router)
 router.include_router(competency_bank_forms_router)
+router.include_router(staff_router)
 
