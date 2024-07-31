b49a6e84c5f40d7724bfb3cba2395b119786b1d6
Jude Clottey <judeclottey1@gmail.com>
2024-07-31 14:13:35 +0000

    Merge GIKPA-246-Create-staff-permissions-repo-service-and-API into dev

--- backend/app/apis/routers.py
+++ backend/app/apis/routers.py
@@ -2,6 +2,7 @@
 from domains.appraisal.apis.appraisal import appraisal_forms_router
 from domains.appraisal.apis.staff_deadline import staff_deadline_router
 from fastapi import APIRouter
+from domains.appraisal.apis.staff_permissions import staff_permission_router
 
 
 
@@ -10,4 +11,6 @@ from fastapi import APIRouter
 router = APIRouter()
 router.include_router(staff_deadline_router)
 
+router.include_router(appraisal_forms_router)
+router.include_router(staff_permission_router)
 
