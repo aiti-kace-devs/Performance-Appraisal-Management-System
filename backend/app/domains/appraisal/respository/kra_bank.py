161c212f317694ed37f68064c9bca301384f7c45
Songtaa <songtaa9@gmail.com>
2024-08-01 13:04:10 +0000

    GIKPA-247-Create-kra-bank-repo-service-and-API

--- backend/app/domains/appraisal/respository/kra_bank.py
+++ backend/app/domains/appraisal/respository/kra_bank.py
@@ -1,10 +1,10 @@
 from crud.base import CRUDBase
-from domains.appraisal.models.appraisal import Appraisal
-from domains.appraisal.schemas.appraisal import (
-    AppraisalCreate, AppraisalUpdate
-)
+from domains.appraisal.models.kra_bank import KraBank
+from domains.appraisal.schemas.kra_bank import KraBankBaseCreate, KraBankUpdate
 
 
-class CRUDAppraisal(CRUDBase[Appraisal, AppraisalCreate, AppraisalUpdate]):
+class CRUDKraBank(CRUDBase[KraBank, KraBankBaseCreate, KraBankUpdate]):
     pass
-appraisal_form_actions = CRUDAppraisal(Appraisal)
+
+
+kra_bank_actions = CRUDKraBank(KraBank)
