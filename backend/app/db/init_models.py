bd0729bcc15b9a36acfc2c4287e2c7b2f81f1231
Songtaa <songtaa9@gmail.com>
2024-08-02 10:35:21 +0000

    wip

--- backend/app/db/init_models.py
+++ backend/app/db/init_models.py
@@ -1,11 +1,15 @@
-from domains.appraisal.models.appraisal import APIBase
-from domains.appraisal.models.appraisal_submission import APIBase
 from domains.appraisal.models.appraisal_configuration import APIBase
 from domains.appraisal.models.appraisal_cycle import APIBase
+from domains.appraisal.models.appraisal_form import APIBase
 from domains.appraisal.models.appraisal_section import APIBase
-
-from domains.appraisal.models.department import APIBase 
+from domains.appraisal.models.appraisal_submission import APIBase
+from domains.appraisal.models.appraisal import APIBase
+from domains.appraisal.models.competency_bank import APIBase
+from domains.appraisal.models.department import APIBase
+from domains.appraisal.models.staff_deadline import APIBase
+from domains.appraisal.models.staff_permissions import APIBase
 from domains.appraisal.models.staff import APIBase
+from domains.appraisal.models.users import APIBase
 from domains.appraisal.models.kra_bank import APIBase
 from db.session import engine
 
@@ -14,3 +18,4 @@ from db.session import engine
 def create_tables():
     APIBase.metadata.create_all(bind=engine)
    
+
