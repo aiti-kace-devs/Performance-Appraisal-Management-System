cbca01a33010f3bcc2dba936690a704de33c5e2b
GI-KACE-DEV <dev.aiti.com.gh@gmail.com>
2024-08-07 09:27:06 +0000

    update

--- backend/app/domains/appraisal/models/appraisal_form.py
+++ backend/app/domains/appraisal/models/appraisal_form.py
@@ -9,13 +9,7 @@ from db.base_class import APIBase
 
 
 
-class Appraisal_form(APIBase):
+class AppraisalForm(APIBase):
     appraisal_id =  Column(UUID(as_uuid=True))
     appraisal_sections_id =  Column(UUID(as_uuid=True))
     form_fields = Column(JSON, nullable=False)
-
-    def serialize(self):
-        return {
-            'created_date': self.created_date,
-            'updated_date': self.updated_date
-        }
