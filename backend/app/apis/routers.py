15621c3d51ad26389684f72079a8919caed04f0c
moro23 <billhab66@gmail.com>
2024-08-08 11:34:22 +0000

    updated apis routers.

--- backend/app/apis/routers.py
+++ backend/app/apis/routers.py
@@ -1,6 +1,7 @@
 
 from domains.appraisal.apis.appraisal import appraisal_forms_router
-# from domains.appraisal.apis.roles import role_router
+from domains.appraisal.apis.roles import role_router
+from domains.appraisal.apis.permissions import perm_router
 from domains.appraisal.apis.role_permission import role_perm_router
 from fastapi import APIRouter
 
@@ -11,6 +12,7 @@ from fastapi import APIRouter
 router = APIRouter()
 router.include_router(appraisal_forms_router)
 router.include_router(role_perm_router)
-# router.include_router(role_router)
+router.include_router(role_router)
+router.include_router(perm_router)
 
 
