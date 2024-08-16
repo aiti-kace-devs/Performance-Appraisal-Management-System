6af6f2dca4b6174248d8129551654e6f193d2e06
GI-KACE-DEV <dev.aiti.com.gh@gmail.com>
2024-08-16 12:39:14 +0000

    wip1

--- backend/app/domains/auth/apis/login.py
+++ backend/app/domains/auth/apis/login.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter,Depends,status,Response,Cookie,Request
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.exceptions import HTTPException
-
-
-
+from domains.auth.schemas import auth as schema
+from sqlalchemy.orm import Session
+from db.session import get_db
 
 
 
@@ -13,3 +13,15 @@ auth_router = APIRouter(
     tags=["Authentication"],
     responses={404: {"description": "Not found"}},
 )
+
+
+
+
+
+
+
+
+@auth_router.post("/refresh", response_model=schema.RefreshTokenResponse)
+def get_new_access_token(response:Response, access_token: schema.Token, db: Session = Depends(get_db)):
+
+    return ""
