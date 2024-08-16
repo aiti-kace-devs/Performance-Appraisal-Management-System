d5ac0b10f83a4bcaa5658fbdf4c93aa6ac5adf5c
GI-KACE-DEV <dev.aiti.com.gh@gmail.com>
2024-08-16 12:50:13 +0000

    wip

--- backend/app/domains/auth/apis/login.py
+++ backend/app/domains/auth/apis/login.py
@@ -4,8 +4,8 @@ from fastapi.exceptions import HTTPException
 from domains.auth.schemas import auth as schema
 from sqlalchemy.orm import Session
 from db.session import get_db
-
-
+from domains.auth.models.refresh_token import RefreshToken
+from domains.auth.services.user_account import users_forms_service
 
 # Authentication module for admins and users
 auth_router = APIRouter(
@@ -23,5 +23,15 @@ auth_router = APIRouter(
 
 @auth_router.post("/refresh", response_model=schema.RefreshTokenResponse)
 def get_new_access_token(response:Response, access_token: schema.Token, db: Session = Depends(get_db)):
+
+    refresh_token_check = db.query(RefreshToken).filter(RefreshToken.refresh_token == access_token.access_token).first()
+    if not refresh_token_check:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                        detail="Could not validate new access token credentials",
+                        headers={"WWW-Authenticate": "Bearer"}
+                        )
+    
+    user_data = users_forms_service.get_user_by_id(db=db, id=refresh_token_check.id)
+    
 
     return ""
