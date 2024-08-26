5351eeb34bf7c480479edb93809f79cb228bdebe
moro23 <billhab66@gmail.com>
2024-08-26 12:08:30 +0000

    updated password_reset service.

--- backend/app/domains/auth/services/password_reset.py
+++ backend/app/domains/auth/services/password_reset.py
@@ -6,15 +6,15 @@ from fastapi import Depends, HTTPException, status, Response, Request
 from sqlalchemy.orm import Session
 from db.session import get_db
 from domains.auth.models.users import User
-from domains.auth.schemas.password_reset import ReResetPasswordRequest
+from domains.auth.schemas.password_reset import ResetPasswordRequest
 
 class PasswordResetService:
 
     def generate_reset_token(self):
         return ''.join(random.choices(string.ascii_letters + string.digits, k=50))
     
-    def get_current_user_email(self, request: Request, db: Session = Depends(get_db)):
-        user = db.query(User).filter(User.email == email).first() 
+    def get_current_user_email(self, schemas: ResetPasswordRequest, db: Session = Depends(get_db)):
+        user = db.query(User).filter(User.email == schemas.email).first() 
 
         if not user:
             raise HTTPException(status_code=404, detail="User not found")
